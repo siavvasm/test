a8f18d6e30572d14b3aaf11e62c8725e7bedce14
Angeliki A. Tsintzira <angeliki.agathi.tsintzira@gmail.com>
2020-12-04 08:48:28 +0000

    Create testClass.java

---
+++ src/main/java/breakingPointTool/artifact/testClass.java
@@ -0,0 +1,19 @@
+package main.java.breakingPointTool.artifact;
+
+// This class contains metrics for Java Language
+public class testClass 
+{
+	private String projectName;
+	private String className;
+  
+  public void testMethod()
+  {
+      for (int i = 0; i < 10; i++)
+        for ( i = 0; i < 10; i++)
+        for ( i = 0; i < 10; i++)
+        for ( i = 0; i < 10; i++)
+        for ( i = 0; i < 10; i++)
+        for ( i = 0; i < 10; i++)
+        System.out.print("Test");
+  }
+}
